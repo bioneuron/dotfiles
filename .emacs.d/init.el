244cbc943724af3c76692be011980890b433a292
Rasoul Azizi <rasoul.azizi@gmail.com>
2020-06-19 07:46:31 +0430

    Ido color customization

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,6 +79,30 @@
 (setq indo-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+
+;;; M-x configurations
+(global-set-key
+     "\M-x"
+     (lambda ()
+       (interactive)
+       (call-interactively
+        (intern
+         (ido-completing-read
+          "M-x "
+          (all-completions "" obarray 'commandp))))))
+
+
+
+;;;; ido color customization
+
+(custom-set-faces
+ ;;'(ido-subdir ((t (:foreground "#66ff00")))) ;; Face used by ido for highlighting subdirs in the alternatives.
+ ;;'(ido-first-match ((t (:foreground "#8b0000")))) ;; Face used by ido for highlighting first match.
+ ;;'(ido-only-match ((t (:foreground "#ffffff")))) ;; Face used by ido for highlighting only match.
+ '(ido-only-match ((t (:background "#008000"))))
+ ;;'(ido-indicator ((t (:foreground "#ffffff")))) ;; Face used by ido for highlighting its indicators (don't actually use this)
+'(ido-incomplete-regexp ((t (:foreground "#0000FF"))))) ;; Ido face for indicating incomplete regexps. (don't use this either)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;-- matlab --;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
